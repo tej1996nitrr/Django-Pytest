d2a61855aadbd0c9881eaf14fb35661644f4ea3e
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-01 21:24:33 +0530

    testing excerpt

--- DjangoPytest/birdie/tests/test_models.py
+++ DjangoPytest/birdie/tests/test_models.py
@@ -6,5 +6,11 @@ class TestPost:
     def test_model(self):
         obj = mixer.blend('birdie.Post')
         assert obj.pk ==1,'Should create a post instance'
-    
+
+    def test_excerpt(self):
+        obj = mixer.blend('birdie.Post', body="Hello World!")
+        result = obj.get_excerpt(5)
+        assert result=="Hello", 'Should return first few characters'
+        
+
     
