704ad1d4d4f7774cd43043c899a9d46831140785
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-05 14:03:13 +0530

    testing authenticated user

--- DjangoPytest/birdie/views.py
+++ DjangoPytest/birdie/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 class HomeView(TemplateView):
     template_name = "birdie/home.html"
+
+
+class AdminView(TemplateView):
+    template_name = "birdie/admin.html"
+    
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(AdminView, self).dispatch(request, *args, **kwargs)
+    
 
